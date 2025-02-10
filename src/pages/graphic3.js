9ddcbe4e82b9c6176c56d1549e3fb2f7d363a939
Callista <callyjchan@gmail.com>
2025-02-10 14:09:26 -0800

    adding another project

--- src/pages/graphic3.js
+++ src/pages/graphic3.js
@@ -8,7 +8,7 @@ export default function Design1() {
     <>
      <h1 className={styles.title}>Petal Purrfections</h1>
         <div className={styles.firstImage}>
-            <Image src='/images/image2.png' alt="picture" width={490} height={570} />
+            <Image src='/images/image10.png' alt="picture" width={490} height={570} />
         </div>
         <div className={styles.writing}>
             <h2>Logo and Poster Design Project</h2>
@@ -18,8 +18,8 @@ export default function Design1() {
             <p>Petal Purrfections is a flower shop where customers can shop for fresh blooms while enjoying the playful company of cats. The logo captures the shop's lively and whimsical spirit, featuring a cat playfully reaching for a flower—a perfect blend of fun and floral charm. The poster designs mirror this playful atmosphere, creatively showcasing the shop’s unique concept while effectively promoting its offerings.</p>
         </div>
         <div className={styles.moreImages}>
-            <Image src='/images/image7.png' alt="another photo" width={1400} height={699}/>
-            <Image src='/images/imagee.png' alt="another photo" width={1400} height={699}/>
+            <Image src='/images/image8.png' alt="another photo" width={1400} height={699}/>
+            <Image src='/images/image9.png' alt="another photo" width={1400} height={699}/>
         </div>
 
     </>
