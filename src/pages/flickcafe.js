5245bb9e04345729d1ca48f9c6da1ccab5e1c0c7
Callista <callyjchan@gmail.com>
2025-02-24 13:08:31 -0800

    flick cafe page

--- src/pages/flickcafe.js
+++ src/pages/flickcafe.js
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar"
 import styles from '@/styles/flickcafe.module.css'
 import Footer from "@/components/Footer";
 import Image from "next/image";
-
+import Link from "next/link";
 export default function FlickCafe() {
     return (
         <>
@@ -12,9 +12,15 @@ export default function FlickCafe() {
         <div className={styles.writing}>
         <h2>UX/UI Design Project</h2>
             <br/>
-            <p>Programs Used: Figma, Javascript</p>
+            <p className={styles.programs}>Programs Used: Figma, Javascript</p>
             <br/>
-        <p>Welcome to Flick Cafe, where the timeless glamour of 1920s art deco meets a modern juxtaposition of cinema and coffee. The name “Flick” is a slang term for film, commonly used in the 1920s, evoking an era where ech reel unfolded stories that captivated hearts and ignited imaginations. Stepping into Flick Cafe is like entering a cinematic time capsule, blending seamlessly with the contemporary buzz of caffeine-fueled conversations and the soft glow of digital screens. Nestled in the heart of Burnaby, Flick Cafe is not just a coffee shop—it's a unique, nostalgic yet modern atmosphere, making it an ideal hangout spot for teens, millennials, and movie lovers of all ages. </p>
+        <p className={styles.blurb}>Welcome to Flick Cafe, where the timeless glamour of 1920s art deco meets a modern juxtaposition of cinema and coffee. The name “Flick” is a slang term for film, commonly used in the 1920s, evoking an era where ech reel unfolded stories that captivated hearts and ignited imaginations. Stepping into Flick Cafe is like entering a cinematic time capsule, blending seamlessly with the contemporary buzz of caffeine-fueled conversations and the soft glow of digital screens. Nestled in the heart of Burnaby, Flick Cafe is not just a coffee shop—it's a unique, nostalgic yet modern atmosphere, making it an ideal hangout spot for teens, millennials, and movie lovers of all ages. </p>
+        <br />
+        <Link className={styles.demo} href='https://flick-cafe.vercel.app/'>Try it out </Link>
+        </div>
+        <div className={styles.otherImgs}>
+        <Image className={styles.menu} src='/images/menu.png' alt="picture" width={490} height={970} />
+        <Image className={styles.screenings} src='/images/Screenings.png' alt="picture" width={490} height={1190} />
         </div>
         <Footer />
         </>
