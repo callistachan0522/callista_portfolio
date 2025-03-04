88b577845fc15d5ebddfd16bb5cae1873961f5f8
Callista <callyjchan@gmail.com>
2025-03-04 10:52:13 -0800

    dark mode fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div className={styles.mainContainer}>
         <main className={styles.container}>
           <div className={styles.mainHeader}>
           <h1 className="home">Callista Chan</h1>
